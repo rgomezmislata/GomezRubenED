98e7f79781ed66efd9cb9794dd719ab174e89adb
rubca <rubcas178@gmail.com>
2022-11-30 16:35:50 +0100

    EX2:AddedGitConflicts

---
+++ src/main/java/ud3/exams/GitConflicts.java
@@ -0,0 +1,17 @@
+package ud3.exams;
+
+import java.util.Scanner;
+import java.util.Locale;
+
+public class GitConflicts {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in).useLocale(Locale.US);
+        int n = in.nextInt();
+        
+        if(n % 2 == 0) {
+            // TODO: Number is even
+        } else {
+            // TODO: Number is odd
+        }
+    }
+}
